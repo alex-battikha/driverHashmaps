49dea3c7db438f436876e68c1d0ddd7e6edac054
1900391 <1900391@01-19962.pusd.dom>
2024-03-26 11:28:07 -0700

    3/26 commit

--- DS/src/westview/ds/DriverHashmaps.java
+++ DS/src/westview/ds/DriverHashmaps.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class DriverHashmaps {
 	
+	//Updated Class
 	public static void main(String[] args) {
 		Scanner inputScanner = new Scanner(System.in);
         System.out.println("Enter Capitalized State Name: ");
@@ -17,28 +18,28 @@ public class DriverHashmaps {
 		HashMap m = new HashMap<String, String>();
 		
 		//adding entries
-		m.put("nice", "mean");
-		m.put("motivated", "lazy");
-		m.put("rich", "poor");
+//		m.put("nice", "mean");
+//		m.put("motivated", "lazy");
+//		m.put("rich", "poor");
 		
 		//query the map for a non-existing key
-		System.out.println(m.get("study"));
-		
-		System.out.println(m.get("rich"));
-		
-		System.out.println(m.size());
-		
-		HashMap m2 = new HashMap<String, ArrayList<String>>();
-		
-		ArrayList<String> food = new ArrayList<String>();
-		food.add("Pasta");
-		food.add("Coffee");
-		food.add("Cake");
-		food.add("Boba");
-		
-		m2.put("Mr. David", "food");
-		
-		System.out.println(m2.get("Mr. David"));
+//		System.out.println(m.get("study"));
+//		
+//		System.out.println(m.get("rich"));
+//		
+//		System.out.println(m.size());
+//		
+//		HashMap m2 = new HashMap<String, ArrayList<String>>();
+//		
+//		ArrayList<String> food = new ArrayList<String>();
+//		food.add("Pasta");
+//		food.add("Coffee");
+//		food.add("Cake");
+//		food.add("Boba");
+//		
+//		m2.put("Mr. David", "food");
+//		
+//		System.out.println(m2.get("Mr. David"));
 		
 		try {
 			System.out.println("Reading...");
